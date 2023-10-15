9c48bc3105c8230cb5eff57501b363c0e7b51cad
Chan <ieejo716@naver.com>
2023-10-15 17:30:05 +0900

    Main에서 Controller 호출 방법 변경
    View 객체를 Controller 객체 생성자로 주입하는 방식으로 전환 -> 테스트 코드 작성하기 어렵다는 사실 인지후 변경

--- Precourse_1/src/main/java/org/example/Main.java
+++ Precourse_1/src/main/java/org/example/Main.java
@@ -1,9 +1,10 @@
 package org.example;
 
 import org.example.controller.ResumeController;
+import org.example.view.ResumeView;
 
 public class Main {
     public static void main(String[] args) {
-        new ResumeController().run();
+        new ResumeController(new ResumeView()).run();
     }
 }
